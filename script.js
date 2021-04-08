b8d34852e5656ca9eff38818d964c37b58c158c5
Violet-K07 <80018055+Violet-K07@users.noreply.github.com>
2021-04-08 11:06:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ function add(a,b){
 console.log(add(20,30));
 console.log(add(100,30));
 
-function showTopic(){
-	var x =document.getElementById("demo");
-	x.style.fontSize="25px";
-	x.style.color="red";
+function showTopic(){ 
+ var x = document.getElementById("demo"); 
+ x.style.fontSize = "25px"; 
+ x.style.color = "red"; 
 }
 
 var veggies=["tomoto","spinach","broccoli"];
